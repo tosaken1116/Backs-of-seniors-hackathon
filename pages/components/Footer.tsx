9b3412fe60555b0747f3ccc3b712358e15ca7540
tosaken1116 <nagato20021122@gmail.com>
2022-07-16 00:38:42 +0900

    #4[udpate] フッターのボタンによる画面遷移を実装

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -3,17 +3,17 @@ import { useRouter } from "next/router"
 export default function Footer(){
     const router =  useRouter()
     return(
-        (router.pathname == '/')?
+        (router.pathname != '/')?
         <div className="sticky bottom-0">
             <div className="grid grid-cols-3">
-                <a className=" border border-black rounded px-6 mx-auto" href='/'>
-                    test
+                <a className=" border border-black rounded px-6 mx-auto" href='/InputCalorie'>
+                    食事を記録
                 </a>
                 <a className=" border border-black rounded px-6 mx-auto" href='/'>
                     test
                 </a>
-                <a className=" border border-black rounded px-6 mx-auto" href='/'>
-                    test
+                <a className=" border border-black rounded px-6 mx-auto" href='/calorieOfThisMonth'>
+                    カロ草
                 </a>
             </div>
         </div>:

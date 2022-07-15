6fefcf0255ec9aa9492ab728e541887bf9f3eb6e
tosaken1116 <nagato20021122@gmail.com>
2022-07-16 00:34:36 +0900

    #3[add] 食事内容を登録するフォームを実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,9 @@ export default function TopPage(){
         <div className="flex flex-col h-screen">
             <Header></Header>
             <div className="flex-grow">
-                <div className="justify-center text-2xl text-center mt-36">アプリ名をRY90
-                入力</div>
+                <div className="justify-center text-2xl text-center mt-36">
+                    アプリ名を入力
+                </div>
                 <div className="flex mt-64 justify-center px-auto">
                     <div className="border mx-auto rounded-xl text-center border-black px-12 py-6 text-2xl">
                         Sign In
---
+++ pages/modal.tsx
@@ -0,0 +1,53 @@
+import Modal from 'react-modal'
+import { useState } from 'react'
+
+const customStyles ={
+    overlay: {
+        position:"fixed",
+        top:0,
+        left:0,
+        backgroundColor:"rgba(0,0,0.3)"
+    },
+
+    content:{
+        top:'50%',
+        left:'50%',
+        right:'50%',
+        bottom:'50%',
+        marginRight:'-50%',
+        width:'500px',
+        height:'300px',
+        transform: 'translate(-50%,-50%)'
+    }
+};
+
+Modal.setAppElement('#__next')
+
+const App =()=>{
+    const [modalIsopen,setIsOpen] = useState(false)
+
+    const openModal = () =>{
+        setIsOpen(true)
+    }
+
+    const afterOpenModal = () =>{
+
+    }
+    const closeModal = () =>{
+        setIsOpen(false)
+    }
+
+    return (
+        <div>
+            <button onClick={openModal}>open modal</button>
+            <Modal
+                isOpen = {modalIsopen}
+
+                onAfterOpen={afterOpenModal}
+
+                onRequestClose={closeModal}
+                style={customStyles}
+            />
+        </div>
+    )
+}

6535f9c8c194dcfc935763aa2ee2985b0e6e0233
tosaken1116 <nagato20021122@gmail.com>
2022-07-16 17:41:40 +0900

    #4[fix] デプロイ時エラー解消

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,22 +1,31 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 
-export default function Footer(){
-    const router =  useRouter()
-    return(
-        (router.pathname != '/')?
+export default function Footer() {
+    const router = useRouter();
+    return router.pathname != "/" ? (
         <div className="sticky bottom-0">
             <div className="grid grid-cols-3">
-                <a className=" border border-black rounded px-6 mx-auto" href='/InputCalorie'>
+                <link
+                    className=" border border-black rounded px-6 mx-auto"
+                    href="/InputCalorie"
+                >
                     食事を記録
-                </a>
-                <a className=" border border-black rounded px-6 mx-auto" href='/'>
+                </link>
+                <link
+                    className=" border border-black rounded px-6 mx-auto"
+                    href="/"
+                >
                     test
-                </a>
-                <a className=" border border-black rounded px-6 mx-auto" href='/calorieOfThisMonth'>
+                </link>
+                <link
+                    className=" border border-black rounded px-6 mx-auto"
+                    href="/calorieOfThisMonth"
+                >
                     カロ草
-                </a>
+                </link>
             </div>
-        </div>:
+        </div>
+    ) : (
         <></>
-    )
+    );
 }

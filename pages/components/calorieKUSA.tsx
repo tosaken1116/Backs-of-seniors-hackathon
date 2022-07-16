037d63b4a25b0fcd7e7d333d0b0022ac93b4672e
tosaken1116 <nagato20021122@gmail.com>
2022-07-16 12:45:04 +0900

    #3[add] カロ草をツイートできるように実装

--- pages/components/calorieKUSA.tsx
+++ pages/components/calorieKUSA.tsx
@@ -1,14 +1,22 @@
 type propsType ={
     calorieOfDay:number,
     calorieAverage:number,
+    date:number,
 }
 
 export default function CalorieOfKUSA(props:propsType){
-    const cellColor=(props.calorieOfDay>props.calorieAverage+props.calorieAverage/2)?" bg-green-500 p-2 m-2":
-    (props.calorieOfDay>props.calorieAverage)?"bg-green-400 p-2 m-2":
-    (props.calorieOfDay>props.calorieAverage-props.calorieAverage/2)?"bg-green-300 p-2 m-2":
-    "bg-green-200 p-2 m-2"
+    const cellColor=
+    (props.calorieOfDay == -1)?" rounded p-3 m-2":
+    (props.calorieOfDay == 0)?" rounded bg-slate-300 p-3 m-2":
+    (props.calorieOfDay>props.calorieAverage+props.calorieAverage/2)?" rounded bg-green-500 p-3 m-2":
+    (props.calorieOfDay>props.calorieAverage)?"rounded bg-green-400 p-3 m-2":
+    (props.calorieOfDay>props.calorieAverage-props.calorieAverage/2)?"rounded bg-green-300 p-3 m-2":
+    "bg-green-200 p-3 m-2"
     return(
-        <div className={cellColor}></div>
+        <div className={cellColor}>
+            <div className="text-xs -ml-2 -mt-3">
+                {(props.date>0&&props.date<32)?props.date:""}
+            </div>
+        </div>
     )
 }

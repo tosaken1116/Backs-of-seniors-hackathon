b82263f6c7d84a38d0d256060659bfd36f16f45f
tosaken1116 <nagato20021122@gmail.com>
2022-07-16 17:16:14 +0900

    #11[add] googleユーザー認証 githubユーザー認証の実装

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,16 +1,17 @@
-export default function Header(){
-    const testbool = true
-    const goalUserCalories =100
-    return(
-        <div className="bg-red-300 text-2xl sticky top-0">
-            <div className=" text-3xl">
-                アプリ名を入力
-            </div>
-            {testbool &&
-                <div className="">
-                    目標カロリー摂取量{goalUserCalories}cal
-                </div>
-            }
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+export default function Header() {
+    const router = useRouter();
+    const testbool = router.pathname != "/";
+    const goalUserCalories = 100;
+    const { data: session } = useSession();
+    console.log(session);
+    return (
+        <div className="bg-red-300 py-8 text-2xl sticky top-0">
+            <div className=" text-3xl">アプリ名を入力</div>
+            {testbool && (
+                <div className="">目標カロリー摂取量{goalUserCalories}cal</div>
+            )}
         </div>
-    )
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,43 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import { useRouter } from "next/router"
-export default function TopPage(){
-    return (
-        <div className="flex flex-col h-screen">
-            <Header></Header>
-            <div className="flex-grow">
-                <div className="justify-center text-2xl text-center mt-36">
-                    アプリ名を入力
-                </div>
-                <div className="flex mt-64 justify-center px-auto">
-                    <div className="border mx-auto rounded-xl text-center border-black px-12 py-6 text-2xl">
-                        Sign In
+import { signIn, useSession } from "next-auth/react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import { useRouter } from "next/router";
+export default function TopPage() {
+    const { data: session } = useSession();
+    console.log(session);
+    const router = useRouter();
+    if (typeof window !== "undefined" && session !== null) {
+        router.replace("/InputCalorie");
+    } else {
+        return (
+            <div className="flex flex-col h-screen">
+                <Header></Header>
+                <div className="flex-grow">
+                    <div className="justify-center text-2xl text-center mt-36">
+                        アプリ名を入力
                     </div>
-                    <div className="border mx-auto rounded-xl text-center border-black px-12 py-6 text-2xl">
-                        Sign Up
+                    <div className="flex mt-64 justify-center px-auto">
+                        <div className="border mx-auto rounded-xl text-center border-black px-12 py-6 text-2xl">
+                            Sign In
+                        </div>
+                        <div className="border mx-auto rounded-xl text-center border-black px-12 py-6 text-2xl">
+                            Sign Up
+                        </div>
                     </div>
+                    {session === null ? (
+                        <div className="mt-12 flex justify-center mx-auto px-auto">
+                            <button
+                                onClick={() => signIn()}
+                                className=" border mx-auto rounded-xl text-center border-black px-12 py-6 text-2xl"
+                            >
+                                他のアカウントでログイン
+                            </button>
+                        </div>
+                    ) : null}
                 </div>
+                <Footer></Footer>
             </div>
-            <Footer></Footer>
-        </div>
-    )
+        );
+    }
 }
